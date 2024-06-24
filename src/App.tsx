1d8704245571c9253caa2baad2172d800beca4b8
yejunian <leeye51456@gmail.com>
2024-06-24 20:00:51 +0900

    디자인: Vanilla Extract 기본 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css';
+import { appContainer, board, buttons } from './App.css';
 
 function App() {
-  return <div>task-app</div>;
+  return (
+    <div className={appContainer}>
+      <div className={board}></div>
+      <div className={buttons}>
+        <button>이 게시판 삭제하기</button>
+        <button>활동 목록 보이기</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
